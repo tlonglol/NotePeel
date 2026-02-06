2221a79ade1da687681f96d39948957411248a7f
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2026-02-06 14:01:15 -0500

    Comments for explanations

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env
+# load environment variables from .env
 load_dotenv()
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
@@ -11,17 +11,18 @@ from fastapi.middleware.cors import CORSMiddleware
 from ocr_service import extract_text
 
 
-app = FastAPI()
+app = FastAPI() # create fastapi app
 
+#cors middlewar, allowing frontend to send request to backend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+# makes a post api endpoint at /ocr
 @app.post("/ocr")
 async def ocr(file: UploadFile = File(...)):
-    contents = await file.read()
-    text = extract_text(contents)
+    contents = await file.read() # reads raw bytes of uploaded image
+    text = extract_text(contents) # calls ocr service to convert bytes to text
     return {"text": text}
--- backend/ocr_service.py
+++ backend/ocr_service.py
@@ -1,11 +1,12 @@
 from google.cloud import vision
 print("OCR SERVICE LOADED")
-client = vision.ImageAnnotatorClient()
+client = vision.ImageAnnotatorClient() 
+# Handles communication with the google cloud vision api using credentials
 
-def extract_text(image_bytes):
-    image = vision.Image(content=image_bytes)
+def extract_text(image_bytes): # Takes in an image as bytes
+    image = vision.Image(content=image_bytes) # wraps bytes into image object
 
-    response = client.document_text_detection(image=image)
+    response = client.document_text_detection(image=image) #calls api to do ocr(detect text in image)
 
     if response.error.message:
         raise Exception(response.error.message)
